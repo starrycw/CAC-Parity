4b4c0aeb1d8820b8ab683669cf0609288ac6e000
starrycw <AronaLink@outlook.com>
2024-09-02 16:35:38 +0800

    SP20240902-1635

--- Verilog/CACParityEncoder/CACParityEncoder.srcs/sources_1/new/OED_ErrorCounter.v
+++ Verilog/CACParityEncoder/CACParityEncoder.srcs/sources_1/new/OED_ErrorCounter.v
@@ -21,6 +21,30 @@
 
 
 module OED_ErrorCounter(
-
+    clk, rst_n, flag_error, flag_bist
     );
+
+    parameter TH_ERRORCNT = 8;
+    parameter BITWIDTH_ERRORCNT = 3;
+    
+    input wire clk, rst_n, flag_error;
+    output reg flag_bist;
+    
+    // cnt
+    reg [BITWIDTH_ERRORCNT - 1 : 0] errorcnt;
+
+    wire flag_bist_wire;
+    assign flag_bist_wire = (errorcnt == TH_ERRORCNT)? 1'b1 : 1'b0;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            errorcnt <= 0;
+            flag_bist <= 1'b0;
+        end
+        else begin
+            errorcnt <= errorcnt + ((~flag_bist_wire) & flag_error);
+            flag_bist <= flag_bist_wire;
+        end
+    end
+
 endmodule
--- Verilog/CACParityEncoder/CACParityEncoder.srcs/sources_1/new/OED_ParityCmpCore.v
+++ Verilog/CACParityEncoder/CACParityEncoder.srcs/sources_1/new/OED_ParityCmpCore.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2024/09/02 14:27:25
+// Create Date: 2024/09/02 15:21:29
 // Design Name: 
-// Module Name: OED_ParityCmp
+// Module Name: OED_ParityCmpCore
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,7 +20,18 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module OED_ParityCmp(
-
+module OED_ParityCmpCore(
+    seq_a, seq_b, flag_error
     );
+
+    parameter BITWIDTH_SEQ = 8;
+
+    input wire [BITWIDTH_SEQ - 1 : 0] seq_a;
+    input wire [BITWIDTH_SEQ - 1 : 0] seq_b;
+    output wire flag_error;
+
+    // cmp core
+    assign flag_error = (seq_a == seq_b) ? (1'b0) : (1'b1);
+
+
 endmodule
